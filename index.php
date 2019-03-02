2a4e6239bcc942c6c8a5263a74491f1f8d0fc285
Carlos Jr <carlosjunior1983@gmail.com>
2019-03-02 20:04:48 -0300

    Criação e validação da tela de login

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <?php 
 
+session_start();
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \cjrweb\Page;
 use \cjrweb\PageAdmin;
+use \cjrweb\Model\User;
  
 
 $app = new Slim();
@@ -13,19 +15,54 @@ $app->config('debug', true);
 
 $app->get('/', function() {
     
- $page = new Page();
-
- $page->setTpl("index");
+	$page = new Page();
+	
+	$page->setTpl("index");
 
 });
  
 $app->get('/admin', function() {
     
- $page = new PageAdmin();
-
- $page->setTpl("index");
+	User::verifyLogin();
+	
+	$page = new PageAdmin();
+	
+	$page->setTpl("index");
 
 });
+
+
+$app->get('/admin/login', function() {
+    
+	$page = new PageAdmin([
+
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("login");
+
+});
+
+$app->post('/admin/login', function(){
+
+	User::login($_POST["login"], $_POST["password"]);
+
+	header("Location: /admin");
+	exit;
+
+}) ;
+
+
+$app->get('/admin/logout', function(){
+
+	User::logout();
+	
+	header("Location: /admin/login");
+	exit;
+
+});
+
 
 $app->run();
 
